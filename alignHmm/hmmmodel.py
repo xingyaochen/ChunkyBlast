ad430d06f8b53626c9d166c08be75a0b3540d825
Nuo Liu <ivliu@Nuos-MacBook-Pro.local>
2018-11-28 14:11:15 -0800

    changed the transition

--- alignHmm/hmmmodel.py
+++ alignHmm/hmmmodel.py
@@ -69,8 +69,8 @@ class HmmModel:
         self.emmit[stateNum] = stateNumD
 
     def calAllTransProb(self):
-        #lets encode begin as M0, end as M_len(self.matchStates)
-        for i in range(len(self.matchStates)):
+        #lets encode begin as M0, end as M_len(self.matchStates)+1
+        for i in range(len(self.matchStates)+1):
             self.calcTransProb(i,i+1)
       
 
@@ -83,22 +83,27 @@ class HmmModel:
         # state: (type, pos), ex: ('M', 0), ('D', 1)
         transitionD = self.tallyTransD(state1Num, state2Num) 
         if state2Num<=len(self.matchStates):
-            allPossibleTrans = [ "M" , "I", "D"]
+            PossibleTransTo = [ "M" , "I", "D"]
         else:
-            allPossibleTrans = [ "M" , "I"]
+            PossibleTransTo = [ "M" , "I"]
+        
+        if state1Num==0:
+            PossibleTransFrom=[ "M" , "I"]
+        else:
+            PossibleTransFrom = [ "M" , "I", "D"]
 
         # calculate all denominator types
         #plus 3 for number of states to transition to
-        demominatorM = sum([transitionD["M"+trans] for trans in allPossibleTrans]) + 3  # sum of MM, MI, MD
+        demominatorM = sum([transitionD["M"+trans] for trans in PossibleTransFrom]) + len(PossibleTransTo)  # sum of MM, MI, MD
         if state1Num!=0:
-            demominatorD = sum([transitionD["D"+trans] for trans in allPossibleTrans]) + 3  # sum of DM, DI, DD
-        demominatorI = sum([transitionD["I"+trans] for trans in allPossibleTrans]) + 3  # sum of IM, II, ID
+            demominatorD = sum([transitionD["D"+trans] for trans in PossibleTransFrom]) + len(PossibleTransTo)  # sum of DM, DI, DD
+        demominatorI = sum([transitionD["I"+trans] for trans in PossibleTransFrom]) + len(PossibleTransTo)  # sum of IM, II, ID
         
         # for-loop to make all possible transition types
-        for i in range(len(allPossibleTrans)):
-            fromState = allPossibleTrans[i]
-            for j in range(len(allPossibleTrans)):
-                toState = allPossibleTrans[j] 
+        for i in range(len(PossibleTransFrom)):
+            fromState = PossibleTransFrom[i]
+            for j in range(len(PossibleTransTo)):
+                toState = PossibleTransTo[j] 
 
                 # depending on fromState, select the demominator to use
                 if fromState == "M":
@@ -134,22 +139,85 @@ class HmmModel:
         tally up all transition types going from match state 1 to match state 2
         """
         transitionD = {}
+        #special edge case for from beginning states to match1 etc.
         if state1Num==0:
             #from begin
+            transitionD = {"II": 0, "IM": 0, "ID":0, "MM":0, "MI":0, "MD":0}
             nextPos = self.matchStates[state2Num-1] 
             state2Col = [seq[1][nextPos] for seq in self.info]
             if nextPos==0:
-                transitionD["II"]=1.0/3
-                transitionD["ID"]=1.0/3
-                transitionD["IM"]=1./3
-                transitionD["MI"]=1.0/(len(self.info)+3)
-                transitionD["MD"]=float(state2Col.count('.')+1)/(len(self.info)+3)
-                transitionD["MM"]=float(len(self.info)-state2Col.count('.')+1)/(len(self.info)+3)
+                transitionD["MD"]=state2Col.count('.')
+                transitionD["MM"]=len(self.info)-state2Col.count('.')
             if nextPos!=0:
-                nextPos = self.matchStates[state2Num-1]
+                
                 initialStateInfo=[seq[1][:nextPos] for seq in self.info]
 
                 ###TODO
+                for i in range(len(self.info)):
+                    #loop through each sequence
+                    stateTransType = ""
+                    insertion=initialStateInfo[i]
+                    numInserts = len(insertion) - insertion.count('.')
+                    aa2 = state2Col[i]
+                    if insertion!=0:
+                        stateTransType ="MI"
+                    elif aa2 == ".":
+                        stateTransType+="MD" 
+                    elif aa2 in aaList:
+                        stateTransType+="MM" 
+
+                    transitionD[stateTransType] += 1
+                    
+                    if aa2 in aaList:
+                        stateTransType ="IM"
+                    elif aa2 == ".":
+                        stateTransType = "ID" 
+                    transitionD[stateTransType] += 1
+                
+                    stateTransType = "II"
+                    if numInserts!=0:
+                        transitionD[stateTransType] += numInserts-1
+                
+            return transitionD
+        
+        if state2Num==len(self.matchStates)+1:
+            #special case for from last match to ending
+            transitionD = {"II": 0, "IM": 0, "MM":0, "MI":0,"DI":0, "DM":0}
+            curPos = self.matchStates[state1Num-1] 
+            state1Col = [seq[1][curPos] for seq in self.info]
+            if curPos==len(self.info[0][1])-1:
+                #if the last matching column is the last column
+                transitionD["DM"]=state1Col.count('.')
+                
+                transitionD["MM"]=len(state1Col)-state1Col.count('.')
+            else:
+                endingStateInfo=[seq[1][curPos+1:] for seq in self.info]
+                for i in range(len(self.info)):
+                    #loop through each sequence
+                    stateTransType = ""
+                    insertion=endingStateInfo[i]
+                    numInserts = len(insertion) - insertion.count('.')
+                    aa1 = state1Col[i]
+                    if insertion!=0:
+                        stateTransType ="MI"
+                    else:
+                        stateTransType+="MM" 
+
+                    transitionD[stateTransType] += 1
+                    
+                    if numInserts!=0:
+                        stateTransType ="IM"
+                        transitionD[stateTransType] += 1
+                
+                    stateTransType = "II"
+                    if numInserts!=0:
+                        transitionD[stateTransType] += numInserts-1
+            return transitionD
+
+
+
+            
+
                 
 
         curPos = self.matchStates[state1Num-1] 
