b6b12d59917b5d608f5c3bcb2772a102dcf13e9f
16xchen <xingyao314@gmail.com>
2018-11-27 16:12:33 -0800

    finished calcEmit

--- alignHmm/hmmmodel.py
+++ alignHmm/hmmmodel.py
@@ -1,4 +1,5 @@
 from fasta import load
+from collections import Counter
 class HmmModel:
     def __init__(self,filename):
         self.filename=filename
@@ -26,7 +27,7 @@ class HmmModel:
                 if info[x][1][i]!='.':
                     count+=1
             if count>=0.5*len(info):
-                matches+=[i]
+                matches.append(i)
         self.matchStates=matches
         return 
 
@@ -35,6 +36,31 @@ class HmmModel:
         calculate the emmision probability for matching state with number stateNum
         fill the entry in the dictionary
         """
+        # assume stateNum indexes self.matchStates
+        seqPosition = self.matchStates[stateNum]
+
+        # get the corresponding column
+        columnData = [seq[1][seqPosition] for seq in self.info]
+
+        # make a dict of aa counts 
+        countsD = Counter(columnData)
+
+        # get the number of gaps, which is excluded in the denominator
+        gaps = countsD.get('.')
+
+        #calulate denominator
+        denominator = len(columnData) + 20
+        if gaps:
+            denominator -= gaps
+        stateNumD = {}
+
+        # put probs in a dictionary
+        for key, val in countsD.items():
+            stateNumD[key] = (val+1)/denominator 
+
+        # put dictionary into meta dictionarys
+        self.emmit[stateNum] = stateNumD
+
     
     def calcTransProb(self,state1,state2 ):
         """
