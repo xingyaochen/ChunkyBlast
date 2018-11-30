c3fc1cad5c4e78c6e69d79b204d30a250fc93056
Nuo Liu <ivliu@Nuos-MacBook-Pro.local>
2018-11-30 15:20:32 -0800

    finished DP

--- alignHmm/hmmAlign.py
+++ alignHmm/hmmAlign.py
@@ -17,48 +17,74 @@ class HmmAlign:
         #searching seq
         seq = self.seq
         states = self.hmmmodel.matchStates
-        #matrices have index (j,i) where j range from 0 to len(states)+1, i ranges from 0 to len(seq)-1
+        #matrices have j rows ,i columns where j range from 0 to len(states)+1, i ranges from 0 to len(seq)-1
         v_m, v_i, v_d = [[None]*(len(seq)+1) for i in range(len(states)+2)], [[None]*len(seq) for i in range(len(states)+2)], [[None]*len(seq) for i in range(len(states)+2)]
         #initialize:
-        #matching state matrix
+
+        #initialize M matrix
         v_m[0][0]={"prob": 0, "prev": None}
-        # for stNum in range(len(states)+1):
-        #     v_m[stNum][0] = {"prob": 0, "prev": None}
-        #     v_i[stNum][0] = {"prob": 0, "prev": None}
-        #     v_d[stNum][0] = {"prob": 0, "prev": None}
-        qxi = 666 
-        for i in range(1, len(seq)-1):
+        for i in range(1,len(seq)+1):
+            #begin state M0 cannot match to sequence nucleotide positions 
+            v_m[0][i]={"prob": float('-inf'), "prev": None}
+        for j in range(1,len(states)+2):
+            #can't match fake position to any matching state
+            v_m[j][0]={"prob": float('-inf'), "prev": None}
+        #end initializing M matrix
+
+        #initialize I matrix:
+        for j in range(len(states)+2):
+            #can't match fake position to any insertion state
+            v_i[j][0]={"prob": float('-inf'), "prev": None}
+        #end initialize I matrix
+
+        #initialize D matrix:
+        for i in range(1,len(seq)+1):
+            #D0 doesn't exit
+            v_m[0][i]={"prob": float('-inf'), "prev": None}
+        for j in range(len(states)+2):
+            #can't match fake position to any insertion state
+            v_d[j][0]={"prob": float('-inf'), "prev": None}
+        #end initializing D matrix
+        qxi=1
+        for i in range(1, len(seq)+1):
             aa = seq[i]
-            for j in range(1, len(states)):
+            for j in range(1, len(states)+2):
+                #already logged
                 e_m = hmmmodel.getEmitProb(st, aa) 
 
-                amm = hmmmodel.getTransitProb(('M', i), ('M', i+1))
-                aim = hmmmodel.getTransitProb(('I', i), ('M', i))
-                adm = hmmmodel.getTransitProb(('D', i), ('M', i+1))
+                amm = hmmmodel.getTransitProb(('M', j-1), ('M', j))
+                aim = hmmmodel.getTransitProb(('I', j-1), ('M', j))
+                adm = hmmmodel.getTransitProb(('D', j-1), ('M', j))
 
-                ami = hmmmodel.getTransitProb(('M', i), ('I', i))
-                aii = hmmmodel.getTransitProb(('I', i), ('I', i))
-                adi = hmmmodel.getTransitProb(('D', i), ('I', i))
+                ami = hmmmodel.getTransitProb(('M', j), ('I', j))
+                aii = hmmmodel.getTransitProb(('I', j), ('I', j))
+                adi = hmmmodel.getTransitProb(('D', j), ('I', j))
 
-                amd = hmmmodel.getTransitProb(('M', i), ('D', i+1))
-                aid = hmmmodel.getTransitProb(('I', i), ('D', i))
-                add = hmmmodel.getTransitProb(('D', i), ('D', i+1))
+                amd = hmmmodel.getTransitProb(('M', j-1), ('D', j))
+                aid = hmmmodel.getTransitProb(('I', i-1), ('D', j))
+                add = hmmmodel.getTransitProb(('D', j-1), ('D', j))
 
                 # for just v_m 
 
                 m_threeProbs = [v_m[j-1][i-1]["prob"] + amm , v_i[j-1][i-1]["prob"] + aim, v_d[j-1][i-1]["prob"] + adm] 
+                i_threeProbs = [v_m[j][i-1]["prob"] + ami , v_i[j][i-1]["prob"] + aii, v_d[j][i-1]["prob"] + adi]
+                d_threeProbs = [v_m[j-1][i]["prob"] + amd , v_i[j-1][i]["prob"] + aid, v_d[j-1][i]["prob"] + add]
 
                 max_prob_m = np.amax(m_threeProbs)
-                prev_state_m = (self.states[np.argmax(m_threeProbs)], i-1, j-1)
+                max_prob_i = np.amax(i_threeProbs)
+                max_prob_d = np.amax(d_threeProbs)
+                prev_state_m = (self.states[np.argmax(m_threeProbs)], j-1, i-1)
+                prev_state_i = (self.states[np.argmax(i_threeProbs)], j, i-1)
+                prev_state_d = (self.states[np.argmax(d_threeProbs)], j-1, i)
                 
-                vm[i][j] = {"prob": math.log(e_m / qxi)  + max_prob_m, "prev": prev_state_m}
+                v_m[i][j] = {"prob": math.log(e_m / qxi)  + max_prob_m, "prev": prev_state_m}
+                v_i[i][j] = {"prob": max_prob_i, "prev": prev_state_i}
+                v_d[i][j] = {"prob": max_prob_d, "prev": prev_state_d}
 
 
-                # TODO: do the same for v_i and v_d
-
-            # TODO: take care of edge cases
+           
             
-        bestScore=v_m[len(states)+1][len(seq)-1]
+        bestScore=v_m[len(states)+1][len(seq)]
         return bestScore
             
         
